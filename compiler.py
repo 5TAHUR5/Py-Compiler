9fea4079a5a16fc682fea6362a21bf5c95b1efbe
123 <salihovtahir2007@gmail.com>
2022-01-28 15:49:08 +0500

    Add compiler.py

--- compiler.py
+++ compiler.py
@@ -2,6 +2,6 @@ import subprocess
 
 
 def run(filename):
-    result = subprocess.run(['cmd', '/c', 'call', 'python', filename],
-                            stdout=subprocess.PIPE, shell=True)
-    return result.stdout.decode('utf-8')
+    result = subprocess.check_output(
+        ['cmd', '/c', 'call', 'python', filename], shell=True)
+    return result.decode('utf-8')
